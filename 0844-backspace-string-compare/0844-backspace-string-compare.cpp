5f79330212cb5814695e80b06d335849a21e5174
Arvind kumar <www.arvindkumar9161679736@gmail.com>
2026-09-06 20:01:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.41%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            // Process s
+            while (i >= 0) {
+
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Process t
+            while (j >= 0) {
+
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Compare current valid characters
+            if (i >= 0 && j >= 0 && s[i] != t[j]) {
+                return false;
+            }
+
+            // One string has a character while other doesn't
+            if ((i >= 0) != (j >= 0)) {
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+};
